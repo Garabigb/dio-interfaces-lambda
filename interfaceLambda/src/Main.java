30ed1758d3a3cabe703dc345f3cd587649636e02
Bruna Oliveira <braoliver@outlook.com>
2025-07-10 14:23:08 -0300

    Interfaces Funcionais

--- interfaceLambda/src/Main.java
+++ interfaceLambda/src/Main.java
@@ -3,6 +3,8 @@ import Interfaces.VideoPlayer;
 import records.User;
 
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -23,6 +25,25 @@ public class Main {
                 new User("Barbara", 85),
                 new User("Carla", 33));
 
+        // nao tem saida
+        var consumer = new Consumer<User>() {
+            @Override
+            public void accept(final User user) {
+                System.out.println(user);
+            }
+        };
+
+        users.forEach(consumer);
+
+        users.forEach(System.out::println);
+
+        printStringValue(User::name, users);
+
+    }
+
+
+    private static void printStringValue(Function<User, String> callback, List<User> user){
+        user.forEach(u->System.out.println(callback.apply(u)));
     }
 
 
---
+++ interfaceLambda/src/records/User.java
@@ -0,0 +1,4 @@
+package records;
+
+public record User(String name, int age) {
+}
